690b1bfd8b490a8eb0c09a9d5af31a1525480e66
Carlos Chacin <chacin.carlos9798@gmail.com>
2017-09-03 19:44:52 -0400

    Sorted MOS app and unity project into folders

---
+++ Unity_Project_Folder/Assets/webSocketClient/Example/EchoTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EchoTest : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+        WebSocket w = new WebSocket(new Uri("ws://127.0.0.1:3000"));
+        yield return StartCoroutine(w.Connect());
+		w.SendString("Hi there");
+		int i=0;
+		while (true)
+		{
+			string reply = w.RecvString();
+			if (reply != null)
+			{
+				Debug.Log ("Received: "+reply);
+				w.SendString("Hi there"+i++);
+			}
+			if (w.error != null)
+			{
+				Debug.LogError ("Error: "+w.error);
+				break;
+			}
+			yield return 0;
+		}
+		w.Close();
+	}
+
+    void update() {
+        Debug.Log(1);
+    }
+
+
+
+}
